18d5c9b6f1e3b9df233a75f8c47068e33a327659
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-01-24 10:00:50 +0100

    The project was moved into the src folder and then Api project was created

---
+++ src/BMJ.Authenticator.Infrastructure/Identity/IdentityResultExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BMJ.Authenticator.Application.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BMJ.Authenticator.Infrastructure.Identity
+{
+    public static class IdentityResultExtensions
+    {
+        public static Result ToApplicationResult(this IdentityResult result)
+        {
+            return result.Succeeded
+                ? Result.Success()  
+                : Result.Failure(result.Errors.Select(e => e.Description));
+        }
+    }
+}
---
+++ src/BMJ.Authenticator.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BMJ.Authenticator.Application.Common.Interfaces;
+using BMJ.Authenticator.Infrastructure.Identity;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
+namespace BMJ.Authenticator.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions options, 
+            IOptions<OperationalStoreOptions> operationalStoreOptions) 
+            : base(options, operationalStoreOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
