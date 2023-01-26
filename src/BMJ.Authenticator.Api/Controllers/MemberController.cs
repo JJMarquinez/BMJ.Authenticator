1d1a1ca3639f25b9d28a03d212256a9e5e02c522
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-01-26 18:35:28 +0100

    MemberController added

---
+++ src/BMJ.Authenticator.Api/Controllers/MemberController.cs
@@ -0,0 +1,17 @@
+﻿using BMJ.Authenticator.Application.UseCases.Login.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMJ.Authenticator.Api.Controllers
+{
+    [Authorize]
+    public class MemberController : ApiControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginCommand loginCommand)
+        { 
+            return Ok(await Mediator.Send(loginCommand));
+        }
+    }
+}
