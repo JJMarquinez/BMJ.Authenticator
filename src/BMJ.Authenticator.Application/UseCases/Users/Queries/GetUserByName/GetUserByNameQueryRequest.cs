079e5f7d53c39065f9cda38b00d9585bbc3697a2
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-02-26 18:09:15 +0100

    The GetUserByNameQueryRequest was added

---
+++ src/BMJ.Authenticator.Application/UseCases/Users/Queries/GetUserByName/GetUserByNameQueryRequest.cs
@@ -0,0 +1,7 @@
+﻿using BMJ.Authenticator.Application.Common.Models.Results;
+using BMJ.Authenticator.Application.UseCases.Users.Queries.GetAllUsers;
+using MediatR;
+
+namespace BMJ.Authenticator.Application.UseCases.Users.Queries.GetUserByName;
+
+public record GetUserByNameQueryRequest : IRequest<ResultDto<UserDto?>>;
