16d17b07f926ca79c06d84a3f0c09375a2555f71
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-02-26 01:56:08 +0100

    UserDto was added

---
+++ src/BMJ.Authenticator.Application/UseCases/Users/Queries/GetAllUsers/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using BMJ.Authenticator.Application.Common.Mappings;
+using BMJ.Authenticator.Domain.Entities.Users;
+
+namespace BMJ.Authenticator.Application.UseCases.Users.Queries.GetAllUsers;
+
+public class UserDto : IMapFrom<User>
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string[] Roles { get; set; }
+}
