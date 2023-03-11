c258fc5fac40776d63a40fdac1a695cfcd9d79b9
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-03-11 15:43:36 +0100

    Telemetry class was added with a main Diagnostic.ActivitySource

---
+++ src/BMJ.Authenticator.Application/Common/Instrumentation/Telemetry.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace BMJ.Authenticator.Application.Common.Instrumentation;
+
+public class Telemetry
+{
+    public static readonly string ServiceName = "BMJ.Authenticator";
+    public static readonly string Version = "1.0.0";
+    public static readonly ActivitySource Source = new(ServiceName, Version);
+}
