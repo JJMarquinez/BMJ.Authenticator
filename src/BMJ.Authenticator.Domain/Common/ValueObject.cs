884c242036deebbf32afb61ec7c965a935f5f830
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-03-19 17:15:02 +0100

    ValueObject's unit tests were added

--- src/BMJ.Authenticator.Domain/Common/ValueObject.cs
+++ src/BMJ.Authenticator.Domain/Common/ValueObject.cs
@@ -1,5 +1,6 @@
 ﻿namespace BMJ.Authenticator.Domain.Common;
 
+// Learn more: https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/microservice-ddd-cqrs-patterns/implement-value-objects
 public abstract class ValueObject
 {
     protected static bool EqualOperator(ValueObject left, ValueObject right)
---
+++ tests/BMJ.Authenticator.Domain.UnitTests/Common/ValueObjectTests.cs
@@ -0,0 +1,70 @@
+﻿using BMJ.Authenticator.Domain.ValueObjects;
+
+namespace BMJ.Authenticator.Domain.UnitTests.Common;
+
+public class ValueObjectTests
+{
+    [Fact]
+    public void Should_BeEqual_When_TheyAreTheSame()
+    {
+        Phone firstPhone = Phone.New("111 222 3333");
+        Phone secondPhone = Phone.New("111 222 3333");
+        Assert.True(firstPhone == secondPhone);
+    }
+
+    [Fact]
+    public void Should_NotBeEqual_When_TheRightOneOfThemIsNull()
+    {   
+        Phone firstPhone = Phone.New("111 222 3333");
+        Phone secondPhone = null!;
+        Assert.False(firstPhone == secondPhone);
+    }
+
+    [Fact]
+    public void Should_NotBeEqual_When_TheLeftOneOfThemIsNull()
+    {
+        Phone firstPhone = null!;
+        Phone secondPhone = Phone.New("111 222 3333");
+        Assert.False(firstPhone == secondPhone);
+    }
+
+    [Fact]
+    public void Should_BeEqual_When_BothOfThemAreNull()
+    {
+        Phone firstPhone = null!;
+        Phone secondPhone = null!;
+        Assert.True(firstPhone == secondPhone);
+    }
+
+    [Fact]
+    public void Should_NotBeDifferent_When_TheyAreTheSame()
+    {
+        Phone firstPhone = Phone.New("111 222 3333");
+        Phone secondPhone = Phone.New("111 222 3333");
+        Assert.False(firstPhone != secondPhone);
+    }
+
+    [Fact]
+    public void Should_BeDifferent_When_TheRightOneOfThemIsNull()
+    {
+        Phone firstPhone = Phone.New("111 222 3333");
+        Phone secondPhone = null!;
+        Assert.True(firstPhone != secondPhone);
+    }
+
+    [Fact]
+    public void Should_BeDifferent_When_TheLeftOneOfThemIsNull()
+    {
+        Phone firstPhone = null!;
+        Phone secondPhone = Phone.New("111 222 3333");
+        Assert.True(firstPhone != secondPhone);
+    }
+
+    [Fact]
+    public void Should_NotBeDifferent_When_BothOfThemAreNull()
+    {
+        Phone firstPhone = null!;
+        Phone secondPhone = null!;
+        Assert.False(firstPhone != secondPhone);
+    }
+}
