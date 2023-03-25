01ceae970c6b623fcef387742263f750e803c342
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-03-25 19:11:33 +0100

    ErrorTests was refactored

--- tests/BMJ.Authenticator.Domain.UnitTests/Common/ErrorTests.cs
+++ tests/BMJ.Authenticator.Domain.UnitTests/Common/ErrorTests.cs
@@ -5,7 +5,6 @@ namespace BMJ.Authenticator.Domain.UnitTests.Common;
 
 public class ErrorTests
 {
-    Error _error;
     string _code;
     string _title;
     string _detail;
@@ -16,16 +15,15 @@ public class ErrorTests
         _title = "User name or password aren't valid.";
         _detail = "The user name or password wich were sent are not correct, either the user doesn't exist or password isn't correct.";
         _httpStatusCode = 409;
-        _error = Error.New(_code, _title, _detail, _httpStatusCode);
     }
 
     [Fact]
-    public void Should_ThrowArgumentNullException_When_IsAttemptToBeCreatedWithNullCode()
+    public void ShouldThrowArgumentNullExceptionGivenNullCode()
     {
         Assert.Throws<ArgumentNullException>(()
             => {
                 Error.New(
-                    null, 
+                    null!, 
                     _title,
                     _detail,
                     _httpStatusCode);
@@ -33,7 +31,7 @@ public class ErrorTests
     }
 
     [Fact]
-    public void Should_ThrowArgumentException_When_IsAttemptToBeCreatedWithEmptyCode()
+    public void ShouldThrowArgumentExceptionGivenEmptyStringAsCode()
     {
         Assert.Throws<ArgumentException>(()
             => {
@@ -46,20 +44,20 @@ public class ErrorTests
     }
 
     [Fact]
-    public void Should_ThrowArgumentNullException_When_IsAttemptToBeCreatedWithNullTitle()
+    public void ShouldThrowArgumentNullExceptionGivenNullTitle()
     {
         Assert.Throws<ArgumentNullException>(()
             => {
                 Error.New(
                     _code,
-                    null,
+                    null!,
                     _detail,
                     _httpStatusCode);
             });
     }
 
     [Fact]
-    public void Should_ThrowArgumentException_When_IsAttemptToBeCreatedWithEmptyTitle()
+    public void ShouldThrowArgumentExceptionGivenEmptyStringAsTitle()
     {
         Assert.Throws<ArgumentException>(()
             => {
@@ -72,20 +70,20 @@ public class ErrorTests
     }
 
     [Fact]
-    public void Should_ThrowArgumentNullException_When_IsAttemptToBeCreatedWithNullDetail()
+    public void ShouldThrowArgumentNullExceptionGivenNullDetail()
     {
         Assert.Throws<ArgumentNullException>(()
             => {
                 Error.New(
                     _code,
                     _title,
-                    null,
+                    null!,
                     _httpStatusCode);
             });
     }
 
     [Fact]
-    public void Should_ThrowArgumentException_When_IsAttemptToBeCreatedWithEmptyDetail()
+    public void ShouldThrowArgumentExceptionGivenEmptyStringDetail()
     {
         Assert.Throws<ArgumentException>(()
             => {
@@ -98,7 +96,7 @@ public class ErrorTests
     }
 
     [Fact]
-    public void Should_ThrowArgumentNullException_When_IsAttemptToBeCreatedWithZeroAsAValueOfHttpStatusCode()
+    public void ShouldThrowArgumentNullExceptionGivenZeroAsfHttpStatusCode()
     {
         Assert.Throws<ArgumentException>(()
             => {
@@ -111,64 +109,84 @@ public class ErrorTests
     }
 
     [Fact]
-    public void Should_CreateNewError_When_ParametersAreValid()
+    public void ShouldCreateNewError()
     {
         Assert.NotNull(Error.New(_code, _title, _detail, _httpStatusCode));
     }
 
-    [Fact]
-    public void Should_GetCode_When_ErrorIsCreated()
+    [Theory]
+    [InlineData("Identity.Argument.UserNameOrPasswordNotValid")]
+    [InlineData("Identity.Argument.UserMustHaveAtLeastOneRole")]
+    [InlineData("Identity.InvalidOperation.ItDoesNotExistAnyUser")]
+    public void ShouldGetCodeGivenCreatedError(string code)
     {
-        Assert.Equal(_code, _error.GetCode());
+        Error error = Error.New(code, _title, _detail, _httpStatusCode);
+        Assert.Equal(code, error.GetCode());
+    }
+
+    [Theory]
+    [InlineData("User was not deleted.")]
+    [InlineData("The user must has at least one role assigned.")]
+    [InlineData("User name or password aren't valid.")]
+    public void ShouldGetTitleGivenCreatedError(string title)
+    {
+        Error error = Error.New(_code, title, _detail, _httpStatusCode);
+        Assert.Equal(title, error.GetTitle());
+    }
+
+    [Theory]
+    [InlineData("Because of internal error the user wasn't deleted, please contact with user administrator.")]
+    [InlineData("The user doesn't have any role assigned and must have at least one.")]
+    [InlineData("The user name or password wich were sent are not correct, either the user doesn't exist or password isn't correct.")]
+    public void ShouldGetDetailGivenCreatedError(string detail)
+    {
+        Error error = Error.New(_code, _title, detail, _httpStatusCode);
+        Assert.Equal(detail, error.GetDetail());
+    }
+
+    [Theory]
+    [InlineData(200)]
+    [InlineData(404)]
+    [InlineData(500)]
+    public void Should_GetHttpStatusCode_When_ErrorIsCreated(int httpStatusCode)
+    {
+        Error error = Error.New(_code, _title, _detail, httpStatusCode);
+        Assert.Equal(httpStatusCode, error.GetHttpStatusCode());
     }
 
     [Fact]
-    public void Should_GetTitle_When_ErrorIsCreated()
+    public void ShouldConvertToString()
     {
-        Assert.Equal(_title, _error.GetTitle());
+        Error error = Error.New(_code, _title, _detail, _httpStatusCode);
+        string errorCode = error;
+        Assert.Equal(error.GetCode(), errorCode);
     }
 
     [Fact]
-    public void Should_GetDetail_When_ErrorIsCreated()
-    {
-        Assert.Equal(_detail, _error.GetDetail());
-    }
-
-    [Fact]
-    public void Should_GetHttpStatusCode_When_ErrorIsCreated()
-    {
-        Assert.Equal(_httpStatusCode, _error.GetHttpStatusCode());
-    }
-
-    [Fact]
-    public void Should_ConvertToString_When_PerformImplicitOperator()
-    {
-        string errorCode = _error;
-        Assert.Equal(_error.GetCode(), errorCode);
-    }
-
-    [Fact]
-    public void Should_BeEqual_When_HasTheSameDataWithOther()
+    public void ShouldBeEqualGivenSameErrors()
     {
         Error none = Error.New("None", "None", "None", 200);
         Assert.True(none.Equals(Error.None));
     }
 
     [Fact]
-    public void Should_NotBeEqual_When_HasDifferentDataWithOther()
+    public void ShouldNotBeEqualGivenDifferentErrors()
     {
-        Assert.False(_error.Equals(Error.None));
+        Error error = Error.New(_code, _title, _detail, _httpStatusCode);
+        Assert.False(error.Equals(Error.None));
     }
 
     [Fact]
-    public void Should_NotBeEqual_When_TheOtherIsNull()
+    public void ShouldNotBeEqualGivenNullToCampareWith()
     {
-        Assert.False(_error.Equals(null));
+        Error error = Error.New(_code, _title, _detail, _httpStatusCode);
+        Assert.False(error.Equals(null));
     }
 
     [Fact]
-    public void Should_NotBeEqual_When_TheOtherIsNotError()
+    public void ShouldNotBeEqualGivenDifferentObjectToCompareWith()
     {
-        Assert.False(_error.Equals(new object()));
+        Error error = Error.New(_code, _title, _detail, _httpStatusCode);
+        Assert.False(error.Equals(new object()));
     }
 }
