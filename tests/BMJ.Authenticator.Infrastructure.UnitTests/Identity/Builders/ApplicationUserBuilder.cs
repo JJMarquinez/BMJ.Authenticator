c0a026f663cee86dfa3d52c339c2c6aefbc97e77
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-04-01 17:20:54 +0200

    ApplicationUser builder was added for applicationUser tests

---
+++ tests/BMJ.Authenticator.Infrastructure.UnitTests/Identity/Builders/ApplicationUserBuilder.cs
@@ -0,0 +1,43 @@
+﻿using BMJ.Authenticator.Infrastructure.Identity;
+
+namespace BMJ.Authenticator.Infrastructure.UnitTests.Identity.Builders;
+
+internal class ApplicationUserBuilder : IApplicationUserBuilder
+{
+    private ApplicationUser _applicationUser = new ApplicationUser();
+    private ApplicationUserBuilder() { }
+
+    public static ApplicationUserBuilder New() => new ApplicationUserBuilder();
+
+    public ApplicationUser Build() => _applicationUser;
+
+    public IApplicationUserBuilder WithEmail(string email)
+    {
+        _applicationUser.Email = email;
+        return this;
+    }
+
+    public IApplicationUserBuilder WithId(string id)
+    {
+        _applicationUser.Id = id;
+        return this;
+    }
+
+    public IApplicationUserBuilder WithPasswordHash(string passwordHash)
+    {
+        _applicationUser.PasswordHash = passwordHash;
+        return this;
+    }
+
+    public IApplicationUserBuilder WithPhoneNumber(string phoneNumber)
+    {
+        _applicationUser.PhoneNumber = phoneNumber;
+        return this;
+    }
+
+    public IApplicationUserBuilder WithUserName(string userName)
+    {
+        _applicationUser.UserName = userName;
+        return this;
+    }
+}
---
+++ tests/BMJ.Authenticator.Infrastructure.UnitTests/Identity/Builders/IApplicationUserBuilder.cs
@@ -0,0 +1,13 @@
+﻿using BMJ.Authenticator.Infrastructure.Identity;
+
+namespace BMJ.Authenticator.Infrastructure.UnitTests.Identity.Builders;
+
+internal interface IApplicationUserBuilder
+{
+    IApplicationUserBuilder WithId(string id);
+    IApplicationUserBuilder WithUserName(string userName);
+    IApplicationUserBuilder WithEmail(string email);
+    IApplicationUserBuilder WithPhoneNumber(string phoneNumber);
+    IApplicationUserBuilder WithPasswordHash(string passwordHash);
+    ApplicationUser Build();
+}
