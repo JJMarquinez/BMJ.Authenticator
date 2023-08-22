3d7db5d30c4c385215e850b65aba3e85408cecab
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-08-22 16:33:58 +0200

    Docker compose file was updated

--- src/BMJ.Authenticator.Application/UseCases/Users/Queries/GetUserById/GetUserByIdQueryRequestValidator.cs
+++ src/BMJ.Authenticator.Application/UseCases/Users/Queries/GetUserById/GetUserByIdQueryRequestValidator.cs
@@ -12,6 +12,6 @@ public class GetUserByIdQueryRequestValidator : AbstractValidator<GetUserByIdQue
         _identityAdapter = identityService;
         RuleFor(v => v.Id)
             .NotEmpty().WithMessage("The Id is mandatory to look for the user.")
-            .Must(_identityAdapter.IsUserIdAssigned).WithMessage("It doesn't exist any user with the Id sent."); ;
+            .Must(_identityAdapter.IsUserIdAssigned).WithMessage("It doesn't exist any user with the Id sent.");
     }
 }
