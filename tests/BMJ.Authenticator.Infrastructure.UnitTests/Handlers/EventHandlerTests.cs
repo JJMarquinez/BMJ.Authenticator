f6d783efa46429a308e0eb01dbea66fd9c4362e0
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-08-23 22:20:32 +0200

    EventHandlerTests was added

---
+++ tests/BMJ.Authenticator.Infrastructure.UnitTests/Handlers/EventHandlerTests.cs
@@ -0,0 +1,75 @@
+﻿using BMJ.Authenticator.Application.Common.Models.Results;
+using BMJ.Authenticator.Domain.ValueObjects;
+using BMJ.Authenticator.Infrastructure.Events;
+using BMJ.Authenticator.Infrastructure.Handlers;
+using MediatR;
+using Microsoft.AspNetCore.OutputCaching;
+using Moq;
+using System;
+
+namespace BMJ.Authenticator.Infrastructure.UnitTests.Handlers;
+
+public class EventHandlerTests
+{
+    private readonly Mock<ISender> _sender;
+    private readonly Mock<IOutputCacheStore> _cache;
+    private readonly IEventHandler _eventHandler;
+
+    public EventHandlerTests()
+    {
+        _sender = new();
+        _sender.Setup(x => x.Send(
+            It.IsAny<IRequest<ResultDto>>(), 
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(ResultDto.NewSuccess);
+
+        _cache = new();
+        _cache.Setup(x => x.EvictByTagAsync(
+            It.IsAny<string>(),
+            It.IsAny<CancellationToken>()
+            )).Returns(new ValueTask());
+
+        _eventHandler = new Infrastructure.Handlers.EventHandler(_sender.Object, _cache.Object);
+    }
+
+    [Fact]
+    public async void ShouldHandleUserCreatedEvent()
+    {
+        var @event = new UserCreatedEvent
+        {
+            UserName = "Joe",
+            Email = "joe@authenticator.com",
+            Phone = "111-222-333",
+            Password = "22*kZ7V8ISl$",
+            Version = 9
+        };
+        var exception = await Record.ExceptionAsync(() => _eventHandler.On(@event));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async void ShouldHandleUserUpdatedEvent()
+    {
+        var @event = new UserUpdatedEvent
+        {
+            UserName = "Joe",
+            Email = "joe@authenticator.com",
+            Phone = "111-222-333",
+            Version = 9
+        };
+        var exception = await Record.ExceptionAsync(() => _eventHandler.On(@event));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async void ShouldHandleUserDeletedEvent()
+    {
+        var @event = new UserDeletedEvent
+        {
+            Id = Guid.NewGuid(),
+            Version = 9
+        };
+        var exception = await Record.ExceptionAsync(() => _eventHandler.On(@event));
+        Assert.Null(exception);
+    }
+}
