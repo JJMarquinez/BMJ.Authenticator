0fbbe8dedf2988a682f5c627436a57b78fcb5072
Jhon Jairo Marquinez Banguera <jhonjairomarban@hotmail.com>
2023-08-25 16:41:13 +0200

    DependencyInjectionTests was added

---
+++ tests/BMJ.Authenticator.Application.UnitTests/DependencyInjection/DependencyInjectionTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using BMJ.Authenticator.Application.Common.Abstractions;
+using BMJ.Authenticator.Application.UseCases.Users.Queries.GetUserById;
+using BMJ.Authenticator.Application.UseCases.Users.Queries.LoginUser;
+using FluentValidation;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BMJ.Authenticator.Application.UnitTests.DependencyInjection;
+
+public class DependencyInjectionTests
+{
+    private readonly IServiceCollection _serviceCollection;
+    public DependencyInjectionTests()
+    {
+        _serviceCollection = new ServiceCollection();
+        _serviceCollection.AddTransient<IIdentityAdapter, IdentityAdapterTest>();
+        _serviceCollection.AddApplicationServices();
+        _serviceCollection.AddScoped(typeof(IPipelineBehavior<,>), typeof(RequestPreProcessorBehavior<,>));
+
+    }
+
+    [Fact]
+    public void ShouldAddAutoMapperServices()
+    {
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+        Assert.IsType<MapperConfiguration>(serviceProvider.GetService<IConfigurationProvider>());
+        Assert.IsType<Mapper>(serviceProvider.GetService<IMapper>());
+    }
+
+    [Fact]
+    public void ShouldAddValidators()
+    {
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+        Assert.IsAssignableFrom<AbstractValidator<GetUserByIdQuery>>(serviceProvider.GetService<GetUserByIdQueryValidator>());
+        Assert.IsAssignableFrom<AbstractValidator<LoginUserQuery>>(serviceProvider.GetService<LoginUserQueryValidator>());
+    }
+
+    [Fact]
+    public void ShouldAddMediatorServices()
+    {
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+        Assert.IsType<Mediator>(serviceProvider.GetService<IMediator>());
+        Assert.IsType<Mediator>(serviceProvider.GetService<ISender>());
+        Assert.IsType<Mediator>(serviceProvider.GetService<IPublisher>());
+    }
+}
